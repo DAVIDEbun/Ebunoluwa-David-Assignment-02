ca6d02b3aa16c576322e3cc492a513b386e943e8
Ebunoluwa  David <81177524+DAVIDEbun@users.noreply.github.com>
2022-07-14 21:36:16 +0000

    I defined the function to calculate the sum of the numbers in the array And also defined the function to count the even numbers in the given array of numbers

--- src/assignment.js
+++ src/assignment.js
@@ -14,9 +14,18 @@ const assignment = {};
  * @returns number the sum of the numbers
  */
 function sumOfNumbers(arrayOfNumbers) {
+  let total = 0
+  for (
+    let i = 0;
+    i < arrayOfNumbers.length;
+    i += 1
+  ) {
+    total += arrayOfNumbers[i];
+  }
+  return total;
     return arrayOfNumbers.length;
 }
-// assignment.sumOfNumbers = sumOfNumbers;
+assignment.sumOfNumbers = sumOfNumbers;
 
 /**
  * Challenge - 2
@@ -28,9 +37,19 @@ function sumOfNumbers(arrayOfNumbers) {
  * @returns number the count of even numbers
  */
 function countEvenNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+  let total = 0
+  for (
+    let i = 0;
+    i< arrayOfNumbers.length;
+    i++
+  ) {
+    if (arrayOfNumbers[i] %2==0)
+     total++
+    }
+  
+    return total;
 }
-// assignment.countEvenNumbers = countEvenNumbers;
+assignment.countEvenNumbers = countEvenNumbers;
 
 /**
  * Challenge - 3
